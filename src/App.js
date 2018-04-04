b223894f18b7315d58639edd842fa2bde0c840a7
Casey Metz <caseymetz@Barberhops-MacBook-Pro.local>
2018-04-03 22:19:52 -0600

    add css modules and decorator syntx

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import styles from "./App.css";
+
+class App extends Component {
+  render() {
+    return <div className={styles.App}>hi</div>;
+  }
+}
+
+export default App;
